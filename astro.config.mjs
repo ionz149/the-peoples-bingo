0108c4842abeb8ad47d683f458c9a20b1df37553
Chris Prieto <ionz149@gmail.com>
2025-12-14 22:15:01 -0800

    config and js path troubleshooting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ionz149.github.io/the-peoples-bingo',
+  base: '/docs',
   outDir: './docs',
   build: {
     assets: 'assets'
   }
 });
+
 
