313d885b82e00a567247e2539e1d0ef9d10a0198
Chris Prieto <ionz149@gmail.com>
2025-12-14 21:57:32 -0800

    config tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ionz149.github.io/the-peoples-bingo/',
   outDir: './docs',
   build: {
     assets: 'assets'
