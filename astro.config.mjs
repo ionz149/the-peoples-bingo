bf42851dbba5427fc501a803fe80dde43bf2030f
Chris Prieto <ionz149@gmail.com>
2025-12-14 21:23:11 -0800

    removal of the dist folder, adding of the docs folder, updating sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './docs',
   vite: {
+
     build: {
-      cssCodeSplit: false
+      cssCodeSplit: false,
     }
   }
-//   build: {
-//     inlineStylesheets: 'never'
-//   }
 });
